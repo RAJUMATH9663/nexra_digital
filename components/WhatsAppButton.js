e43cf315768d26ac1749ff0aae2d7a521d52ed03
Raju Mathapati <chetanbcabbacollegevijayapura@gmail.com>
2026-07-14 16:37:39 +0530

    Update WhatsApp floating button number and add prefilled text

--- components/WhatsAppButton.js
+++ components/WhatsAppButton.js
@@ -4,7 +4,7 @@ import styles from './WhatsAppButton.module.css';
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/911234567890"
+      href="https://wa.me/918618914432?text=how%20can%20i%20help%20u%20%3F"
       target="_blank"
       rel="noopener noreferrer"
       className={styles.fab}
